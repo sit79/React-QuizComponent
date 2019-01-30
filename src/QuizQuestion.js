7163f384c6b025f3e611d0a5d089a159d051816f
sit79 <stefan.tolksdorf@gmail.com>
2019-01-30 11:24:07 +0100

    QuizEnd component created

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import QuizQuestionButton from './QuizQuestionButton.js';
+import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
   render() {
